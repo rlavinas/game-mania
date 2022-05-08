f295d3ac6f2c49677dc0cc2e106fae8ff3aad1e0
rafaellaivnas <rafael.lavinas@korusconsultoria.com.br>
2022-05-08 19:28:17 -0300

    UPG: Criação página incial e login

--- Readme.md
+++ Readme.md
@@ -3,3 +3,7 @@
   https://www.figma.com/file/6wtFddFDDpWUpvh5m8X7ID/Game-Mania-(Rafe)?node-id=27%3A495
   
 # Pagina de exemplo para estudo dos elementos HTML e CSS
+
+# Paginas ciradas 08/05/2022
+  - index.html (página principal).
+  - login.html (página de login).
--- js/script.js
+++ js/script.js
@@ -9,14 +9,64 @@ function AbreMenu() {
 }
 
 function myFunction(x) {
-    document.getElementById("div_menubar").style.display = 'none';
-    document.getElementById("navbar").style.display = 'none';
-    if (x.matches) { // If media query matches
-        document.getElementById("div_menubar").style.display = 'flex';
-        document.getElementById("navbar").style.display = 'flex';
+    if (document.getElementById("div_menubar") != null) {
+        document.getElementById("div_menubar").style.display = 'none';
+        document.getElementById("navbar").style.display = 'none';
+        if (x.matches) { // If media query matches
+            document.getElementById("div_menubar").style.display = 'flex';
+            document.getElementById("navbar").style.display = 'flex';
+        }
     }
 }
   
 var x = window.matchMedia("(min-width: 768px)");
 myFunction(x);
 x.addListener(myFunction);
+
+if (document.getElementById("edtEmail") != null) {
+
+    function limpMsg() {
+        $("#msgErro").html("");
+    }
+
+    var email = document.getElementById("edtEmail");
+
+    email.addEventListener("click", limpMsg);
+    email.addEventListener("mouseover", limpMsg);
+}
+
+if (document.getElementById("edtSenha") != null) {
+
+    function limpMsg() {
+        $("#msgErro").html("");
+    }
+
+    var email = document.getElementById("edtSenha");
+
+    email.addEventListener("click", limpMsg);
+    email.addEventListener("mouseover", limpMsg);
+}
+
+function envLogin(){
+    if (valLogin()) {
+        $("#formLogin").submit();
+    };
+};
+
+function valLogin() {
+var lret = true;
+
+    if ($("#edtEmail").val() == "") {
+        $("#msgErro").html("Necessário informar o e-mail.");
+        lret = false;
+    }
+
+    if (lret)  {
+        if ($("#edtSenha").val() == "") {
+            $("#msgErro").html("Necessário informar a senha.");
+            lret = false;
+        }   
+    }
+
+    return (lret);
+};
